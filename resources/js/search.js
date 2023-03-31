cbdbb3c42e8ea47ec8af2415d483cdb532f6e100
Kelisnor <Kelisnor_Able@Outlook.com>
2023-03-31 17:19:13 -0400

    add a word search funciton to js file

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,45 +1,44 @@
-// Wait for the DOM to finish loading before running the code
-document.addEventListener('DOMContentLoaded', function() {
+function searchLinks(links, query) {
+  // Split the search query into individual words
+  const words = query.toLowerCase().split(' ');
 
-    // Find the search bar element in the DOM
-    const searchBar = document.querySelector('#search-bar');
-  
-    // Find the search form element in the DOM
-    const searchForm = document.querySelector('#search-form');
-  
-    // Find the search results element in the DOM
-    const searchResults = document.querySelector('#search-results');
-  
-    // Add an event listener to the search form
-    searchForm.addEventListener('submit', function(event) {
-  
-      // Prevent the form from submitting normally
-      event.preventDefault();
-  
-      // Get the value of the search bar
-      const searchQuery = searchBar.value.trim();
-  
-      // Send an AJAX request to the server to search for links
-      fetch(`/search?q=${searchQuery}`)
-        .then(response => response.json())
-        .then(data => {
-          // Clear the search results element
-          searchResults.innerHTML = '';
-  
-          // If there are no links, display a message saying so
-          if (data.links.length === 0) {
-            searchResults.innerHTML = '<p>No results found</p>';
-            return;
-          }
-  
-          // For each link, create a new <a> element and append it to the search results
-          data.links.forEach(link => {
-            const linkElement = document.createElement('a');
-            linkElement.href = link.url;
-            linkElement.textContent = link.title;
-            searchResults.appendChild(linkElement);
-          });
-        });
-    });
+  // Filter the links based on whether they contain all of the words in the search query
+  return links.filter(link => {
+    const title = link.title.toLowerCase();
+    const url = link.url.toLowerCase();
+    return words.every(word => title.includes(word) || url.includes(word));
   });
-  
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const searchBar = document.querySelector('#search-bar');
+  const searchButton = document.querySelector('#search-button');
+  const searchForm = document.querySelector('#search-form');
+  const searchResults = document.querySelector('#search-results');
+
+  searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const query = searchBar.value.trim();
+
+    fetch(`/search?q=${query}`)
+      .then(response => response.json())
+      .then(data => {
+        const filteredLinks = searchLinks(data.links, query);
+
+        searchResults.innerHTML = '';
+
+        if (filteredLinks.length === 0) {
+          searchResults.innerHTML = '<p>No results found</p>';
+          return;
+        }
+
+        filteredLinks.forEach(link => {
+          const linkElement = document.createElement('a');
+          linkElement.href = link.url;
+          linkElement.textContent = link.title;
+          searchResults.appendChild(linkElement);
+        });
+      });
+  });
+});
