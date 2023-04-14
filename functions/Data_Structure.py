392368a04d2655c78e8402e9800b49037f877e9a
Tiger Weng <wengj3@rpi.edu>
2023-04-14 16:15:12 -0400

    Donation

---
+++ functions/Data_Structure.py
@@ -0,0 +1,49 @@
+tree = {
+    'Math':{
+        'Algebra':{
+
+        },
+        'Analysis':{
+
+        },
+        'Differential Equations':{
+
+        },
+        'Elementry Definition':{
+
+        },
+        'Geometry':{
+
+        },
+        'Set Theorery':{
+
+        },
+        'Statistics':{
+
+        },
+        'Toplogy':{
+
+        },
+        'Trignometry':{
+
+        },
+    },
+    'Physics':{
+
+    },
+
+    'Elementary Math': {
+        'Algebra': {
+            'Pre-Calculus': {
+                'Calculus': {
+                    'Multivariable Calculus': {
+                        'Linear Algebra': {}
+                    },
+                    'Differential Equations': {}
+                }
+            }
+        },
+        'Geometry': {},
+        'Trigonometry': {}
+    }
+}
